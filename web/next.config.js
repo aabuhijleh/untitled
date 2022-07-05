0e2523100e6c6d675f09303624fcd1f3b819ece8
aabuhijleh <aabuhijleh.dev@gmail.com>
2022-07-05 09:25:18 +0300

    chore: add ability to import '.graphql' files

--- web/next.config.js
+++ web/next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      use: "graphql-tag/loader",
+      exclude: /node_modules/,
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
